5aae24a19be3b8011b6320c85eb32f6368baa2e1
Holger <s164143@student.dtu.dk>
2021-11-24 16:26:09 +0100

    PyTorch Network and PyTorch DE

---
+++ DE_Pytorch.py
@@ -0,0 +1,134 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Network import NeuralNetwork
+from datetime import datetime
+import copy
+import torch
+
+
+class DE:
+
+    def __init__(self, objective_function, population_function, X, y, Xtest=None, ytest=None, pop_size=50,
+                 F=0.5, cr=0.5, start_agent=None):
+        self.obj = objective_function
+        self.X = X
+        self.y = y
+        self.N = pop_size
+        self.F = F
+        self.cr = cr
+        self.pop = population_function(pop_size)
+        self.testNN = population_function(1)[0]
+        if start_agent is not None:
+            self.pop[0] = copy.deepcopy(start_agent)
+        if Xtest is not None and ytest is not None:
+            self.Xtest = Xtest
+            self.ytest = ytest
+            self.testcost = True
+
+    def NN_obj(self, agent):
+        yhat = agent(self.X)[0].T
+        return self.obj(yhat, self.y.long())
+
+    def mutation(self, nets):
+
+        nets.insert(0, self.testNN)
+        for testp, p1, p2, p3 in zip(*[net.parameters() for net in nets]):
+            testp.data = p1 + self.F * (p2 - p3)
+
+        pass
+
+    def crossover(self, target):
+
+        for dw, tw in zip(self.testNN.parameters(), target.parameters()):
+            r = torch.rand(dw.shape)
+            crit = r < self.cr
+            trial_w = crit * dw + ~crit * tw
+            dw.data = trial_w
+
+        pass
+
+    def evolution(self, num_epochs, verbose=False, print_epoch=1000):
+        # evaluate the initialized population with the objective function
+        obj_all = [self.NN_obj(agent) for agent in self.pop]
+
+        # find the best agent within the initial population
+        self.best_agent = self.pop[np.argmin(obj_all)]
+
+        best_obj = min(obj_all)
+        prev_obj = best_obj
+
+        self.best_objs = np.zeros(num_epochs + 1)
+        self.best_objs[0] = best_obj
+
+        if self.testcost:
+            self.best_test_objs = np.zeros(num_epochs + 1)
+            self.best_test_objs[0] = self.obj([self.ytest, self.best_agent.feedforward(self.Xtest)])
+
+        for i in range(num_epochs):
+            for j in range(self.N):
+
+                # Random sampling from the set of all agents exluding the current one, j
+                x = self.pop[j]
+                a, b, c = self.pop[np.random.choice(np.delete(np.arange(self.N), j), 3, replace=False)]
+
+                # Mutation
+                self.mutation([a, b, c])
+
+                # Crossover
+                self.crossover(x)
+
+                # Selection
+                obj_u = self.NN_obj(self.testNN)
+                if obj_u < self.NN_obj(x):
+                    self.pop[j] = copy.deepcopy(self.testNN)
+                    obj_all[j] = obj_u
+
+            # update the current best objective function value
+            best_obj = min(obj_all)
+            self.best_objs[i + 1] = best_obj
+
+            if best_obj < prev_obj:
+                # update best agent
+                self.best_agent = self.pop[np.argmin(obj_all)]
+                # update previous solution to use for next iteration
+                prev_obj = best_obj
+
+            if self.testcost:
+                self.best_test_objs[i + 1] = self.obj(self.best_agent(self.Xtest)[0].T, self.y)
+
+            if verbose and i % print_epoch == 0:
+                # report progress at each iteration
+                print('%d: cost= %.5f' % (i, best_obj))
+                print('%d: testcost= %.5f' % (i, self.best_test_objs[i + 1]))
+                print()
+
+        return self.best_agent
+
+    def evaluate(self, plot_function=None, agent=None, bounds=None, title=' '):
+
+        if agent is None:
+            agent = self.best_agent
+            plt.figure(figsize=(13, 8))
+            plt.plot(range(len(self.best_objs)), self.best_objs)
+            plt.plot(range(len(self.best_test_objs)), self.best_test_objs)
+            plt.title('Training Graph', fontsize=24)
+            plt.xlabel('Iterations', fontsize=20)
+            plt.ylabel('Cost', fontsize=20)
+            plt.legend(['Train', 'Test'], fontsize=14)
+            plt.show()
+
+        if plot_function is not None:
+            plot_function(agent, self.Xtest, self.ytest, title=title, savefig=False)
+
+        print(f"Best agent is {agent} with a train cost of {np.round(self.NN_obj(agent), 5)}.")
+        print(f"And a test cost of {np.round(self.obj([self.ytest, agent.feedforward(self.Xtest)]), 5)}")
+
+        # print(f"Worst initialization was {self.initial_worst_agent} with a cost of \
+        # {np.round(self.obj(self.initial_worst_agent), 2)}.")
+
+        pass
+
+
+
+
+
--- NetworkPytorch.py
+++ NetworkPytorch.py
@@ -1,23 +1,152 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn
 import numpy as np
 import matplotlib.pyplot as plt
 
+def compute_loss(model, X, y, criterion):
+    output = model(X)[0].T
+    loss = criterion(output, y)
+    return loss.item()
 
-class Network(nn.Module):
+def train_loop(model, optimizer, cost, Xtrain, ytrain, Xtest=None, ytest=None, epochs=100,
+                        use_cuda=False, eval=False, plot_iteration=1000):
 
-    def __init__(self, sizes):
-        super().__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(sizes[0], sizes[1]),
-            nn.ReLU(),
-            nn.Linear(sizes[1], sizes[2]),
-            nn.Softmax()
-        )
+    # device = torch.device('cuda') if torch.cuda_available else torch.device('cpu')
+    if torch.cuda.is_available and use_cuda:
+        device = torch.device('cuda')
+        print('Using GPU')
+    else:
+        device = torch.device('cpu')
+        print('Using CPU')
 
-        def forward(self, x):
-            return self.seq(x)
+    Xtrain, ytrain = Xtrain.to(device), ytrain.to(device)
+    Xtest, ytest = Xtest.to(device), ytest.to(device)
+
+    epoch_losses_train = torch.zeros(epochs)
+    epoch_losses_test = torch.zeros(epochs)
+
+    for i in range(epochs):
+        model.train()
+        inputs = Xtrain #.view(1, 1, -1).float()
+        labels = ytrain
+
+        optimizer.zero_grad()
+
+        output = model(inputs)[0].T
+        loss = cost(output, labels.long())
+        loss.backward()
+        optimizer.step()
+
+        if eval:
+            model.eval()
+            with torch.no_grad():
+
+                test_loss = compute_loss(model, Xtest,
+                                         ytest.long(), cost)
+
+                epoch_losses_train[i] = loss.item()
+                epoch_losses_test[i] = test_loss
+
+                if i % plot_iteration == 0:
+
+                    print("Epoch %d: \n Train Loss: %s \n Test Loss: %s" %
+                        (i, loss.item(), test_loss))
+
+                    plt.plot(list(model.parameters())[0].detach()[0][0])
+                    # plt.plot(list(NN.parameters())[1].detach())
+                    plt.show()
+                    #print(loss.item())
+
+    return epoch_losses_test, epoch_losses_train
+
+
+def train_loop_minibatch(model, optimizer, cost, Xtrain, ytrain, Xtest=None, ytest=None, batch_size=200, epochs=100,
+                        use_cuda=False, eval=False, plot_iteration=1000):
+
+    # device = torch.device('cuda') if torch.cuda_available else torch.device('cpu')
+    if torch.cuda.is_available and use_cuda:
+        device = torch.device('cuda')
+        print('Using GPU')
+    else:
+        device = torch.device('cpu')
+        print('Using CPU')
+
+    Xtrain, ytrain = Xtrain.to(device), ytrain.to(device)
+    Xtest, ytest = Xtest.to(device), ytest.to(device)
+
+    iterations_per_epoch = Xtrain.shape[0] // batch_size
+    idx = torch.arange(Xtrain.shape[0])
+
+    epoch_losses_train = torch.zeros(epochs)
+    epoch_losses_test = torch.zeros(epochs)
+
+    for i in range(epochs):
+        model.train()
+        for j in range(iterations_per_epoch):
+            print(j)
+            if batch_size == Xtrain.shape[0]:
+                Xbatch = Xtrain.view(1, 1, -1).float()
+                ybatch = ytrain
+            else:
+                #  chosen = np.random.choice(idx, batch_size, replace=False)
+                chosen = torch.multinomial(idx.float(), batch_size, replacement=False)
+                Xbatch = Xtrain[chosen].view(1, 1, -1).float()  # reshape and cast to float so PyTorch understands it
+                ybatch = ytrain[chosen//8]
+
+            optimizer.zero_grad()
+
+            output = model(Xbatch)[0].T
+            loss = cost(output, ybatch.long())
+            loss.backward()
+            optimizer.step()
+
+        if eval:
+            model.eval()
+            with torch.no_grad():
+
+                train_loss = compute_loss(model, Xtrain.view(1, 1, -1).float(),
+                                          ytrain.long(), cost)
+                test_loss = compute_loss(model, Xtest.view(1, 1, -1).float(),
+                                         ytest.long(), cost)
+
+                epoch_losses_train[i] = train_loss
+                epoch_losses_test[i] = test_loss
+
+                if i % 1000 == 0:
+
+                    print("Epoch %d: \n Train Loss: %s \n Test Loss: %s" %
+                        (i, train_loss, test_loss))
+
+                if i % plot_iteration == 0:
+                    plt.plot(list(model.parameters())[0].detach()[0][0])
+                    # plt.plot(list(NN.parameters())[1].detach())
+                    plt.show()
+                    print(loss.item())
+
+    return epoch_losses_test, epoch_losses_train
+
+
+def train_loop_old(model, optimizer, criterion, X, y, epochs=1000, v=False, plot_iteration=1000):
+    X = X.view(1, 1, -1).float()
+
+    for i in range(epochs):
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        output = model(X)[0].T
+        labels = y.long()
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+
+        if v:
+            if i % plot_iteration == 0:
+                plt.plot(list(model.parameters())[0].detach()[0][0])
+                # plt.plot(list(NN.parameters())[1].detach())
+                plt.show()
+                print(loss.item())
+
+
+
 
 
 
