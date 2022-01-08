be8ea27715db45ff12d7966f7f851c5d3fa8e5ca
FaxMan1 <s164143@student.dtu.dk>
2022-01-08 16:25:25 +0100

    .

--- filters.py
+++ filters.py
@@ -1,5 +1,6 @@
 from scipy import signal
 import scipy
+import torch
 
 def butter_lowpass(cutoff_freq, sampling_rate, order=4):
 
@@ -15,4 +16,11 @@ def ideal_lowpass(signal, cutoff_freq, sampling_rate):
     Tx_low = scipy.fft.irfft(Tx_freq, n=signal.shape[0])
     return Tx_low
 
+def ideal_lowpass_torch(signal, cutoff_freq, sample_rate):
+    signal = signal.view(1,1,-1)
+    Tx_freq = torch.fft.rfft(signal)
+    xf = torch.fft.rfftfreq(signal.shape[2], 1 / sample_rate)
+    Tx_freq[0][0][xf > cutoff_freq] = 0
+    Tx_low = torch.fft.irfft(Tx_freq, n=signal.shape[2])
+    return Tx_low
 
